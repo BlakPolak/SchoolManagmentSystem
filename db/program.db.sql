c83daf464ec31c46c14b93bf4c6d29831dbf7f27
pawellasota <plasota76@gmail.com>
2017-03-20 23:48:40 +0100

    Base structure for sql_alchemy added in db_alchemy, login with sql_alchemy added

--- db/program.db.sql
+++ db/program.db.sql
@@ -10,41 +10,37 @@ CREATE TABLE "User" (
 	`Password`	TEXT,
 	`User_type`	TEXT
 );
-INSERT INTO `User` (ID,Name,Surname,Gender,Birth_date,Email,Login,Password,User_type) VALUES (1,'Jerzy','Mardaus','male','1986-12-12','jerzy.mardaus@codecool.com','jerzy.mardaus@codecool.com','password','manager'),
- (2,'Miriam','Codecool','female','1986-12-12','miriam@codecool.com','miriam@codecool.com','password','employee'),
- (3,'Kati','Codecool','female','1986-12-12','kati@codecool.com','kati@codecool.com','password','employee'),
- (4,'Pawel','Polakiewicz','male','1986-12-12','pawelp@codecool.com','pawelp@codecool.com','password','student'),
- (5,'Ika','Grabon','female','1986-12-12','ika@codecool.com','ika@codecool.com','password','student'),
- (6,'Marta','Sajdak ','female','1986-12-12','marta@codecool.com','marta@codecool.com','password','student'),
- (11,'Rafal','Stepien','male','1986-12-12','rafals@codecool.com','rafals@codecool.com','password','mentor'),
- (12,'Mateusz','Ostafil','male','1986-12-12','mateuszo@codecool.com','mati','pass','mentor'),
- (13,'Marcin','Izworski','male','1986-12-12','marcini@codecool.com','marcini@codecool.com','password','mentor'),
- (14,'Przemek','Ciacka','male','1986-12-12','przemekc@codecool.com','przemekc@codecool.com','password','mentor'),
- (15,'dfs','sdfds','male','1898-12-12','qwewqe@wp.pl','dsaa','asdsad','student'),
- (18,'aaaaa','aaaaa','male','1234-34-43','dsad','asdsa','dsada','student');
-CREATE TABLE "Teams" (
-	`ID`	INTEGER,
-	`Team_name`	TEXT,
-	`ID_Student`	INTEGER,
-	PRIMARY KEY(ID)
+INSERT INTO `User` (ID,Name,Surname,Gender,Birth_date,Email,Login,Password,User_type) VALUES (1,'Jerzy','Mardaus','male','1986-12-12','jerzy.mardaus@codecool.com','jurek','password','manager'),
+ (2,'Miriam','Codecool','female','1986-12-12','miriam@codecool.com','miriam','password','employee'),
+ (3,'Kati','Codecool','female','1986-12-12','kati@codecool.com','kati','password','employee'),
+ (5,'Ika','Grabon','female','1986-12-12','ika@codecool.com','ika','password','student'),
+ (6,'Marta','Sajdak ','female','1986-12-12','marta@codecool.com','marta','password','student'),
+ (11,'Rafal','Stepien','male','1986-12-12','rafals@codecool.com','rafals','haslo','mentor'),
+ (12,'Mateusz','Ostafil','male','1990-01-01','mati@wp.pl','mateo','pass','mentor'),
+ (13,'Marcin','Izworski','male','1986-12-12','marcini@codecool.com','marcin','password','mentor'),
+ (17,'Zbyszek','Wodecki','male','1990-01-01','zbych@wp.pl','zbychu','pass','mentor'),
+ (18,'Roman','Polanski','male','1990-01-01','romek@wp.pl','roman','pass','student');
+CREATE TABLE "Team" (
+	`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name`	TEXT,
+	`ID_Student`	INTEGER
 );
-INSERT INTO `Teams` (ID,Team_name,ID_Student) VALUES (2,'rozmaryn',8),
- (6,'xxx',6),
- (7,'Jakkolwiek',15),
- (10,'oops',4),
- (11,'ZZZ',18),
- (12,'Fistaszki',5);
+INSERT INTO `Team` (ID,name,ID_Student) VALUES (40,'team2','<empty>');
 CREATE TABLE "Submission" (
 	`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`ID_Student`	INTEGER,
 	`ID_Assignment`	INTEGER,
 	`Result`	TEXT,
 	`Grade`	INTEGER,
-	`Submittion_date`	TEXT,
-	`ID_mentor`	INTEGER
+	`date`	TEXT,
+	`ID_Mentor`	INTEGER
 );
-INSERT INTO `Submission` (ID,ID_Student,ID_Assignment,Result,Grade,Submittion_date,ID_mentor) VALUES (1,5,1,'result',4,'2016-12-12',12);
-CREATE TABLE "Checkpoint_submittion" (
+INSERT INTO `Submission` (ID,ID_Student,ID_Assignment,Result,Grade,date,ID_Mentor) VALUES (21,5,1,'Beautifull dojo app',NULL,'2017-03-10',NULL),
+ (22,6,2,'Program in python',4,'2017-03-10',NULL),
+ (23,18,3,'Javascript functions ',4,'2017-03-10',NULL),
+ (24,5,2,'submitted assignment',4,'2017-03-10',NULL),
+ (25,5,6,'result for nowy',NULL,'2017-03-10',NULL);
+CREATE TABLE "Checkpoint_submission" (
 	`ID`	INTEGER,
 	`ID_Student`	INTEGER,
 	`Date`	TEXT,
@@ -53,53 +49,60 @@ CREATE TABLE "Checkpoint_submittion" (
 	`ID_Assignment`	INTEGER,
 	PRIMARY KEY(`ID`)
 );
-INSERT INTO `Checkpoint_submittion` (ID,ID_Student,Date,Card,ID_Mentor,ID_Assignment) VALUES (1,4,'2017-02-10','red',12,5),
- (2,4,'2017-02-10','yellow',12,2),
- (3,5,'2017-02-10','red',12,2);
+INSERT INTO `Checkpoint_submission` (ID,ID_Student,Date,Card,ID_Mentor,ID_Assignment) VALUES (1,5,'2017-03-10','yellow',12,1),
+ (2,5,NULL,NULL,NULL,2),
+ (3,5,'2017-03-10','red',12,3),
+ (4,6,'2017-03-10','red',12,1),
+ (5,6,NULL,NULL,NULL,2),
+ (6,6,'2017-03-10','green',12,3),
+ (7,18,'2017-03-10','green',12,1),
+ (8,18,NULL,NULL,NULL,2),
+ (9,18,'2017-03-10','green',12,3);
 CREATE TABLE "Checkpoint_assignment" (
 	`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`Name`	TEXT,
 	`Assignment`	TEXT
 );
-INSERT INTO `Checkpoint_assignment` (ID,Name,Assignment) VALUES (2,'Procedural programming','main(), procedures'),
- (5,'OOP','classes, init');
+INSERT INTO `Checkpoint_assignment` (ID,Name,Assignment) VALUES (1,'Checkpoint1','todo'),
+ (2,'Checkpoint2','python basic'),
+ (3,'Checkpoint3','java');
 CREATE TABLE "Attendance" (
-	`ID`	INTEGER,
+	`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`ID_Student`	INTEGER,
 	`Date`	TEXT,
-	`Presence`	INTEGER,
-	PRIMARY KEY(ID)
+	`Presence`	INTEGER
 );
-INSERT INTO `Attendance` (ID,ID_Student,Date,Presence) VALUES (1,4,'2017-02-09',1),
- (2,5,'2017-02-09',0),
- (3,6,'2017-02-09',1),
- (4,15,'2017-02-09',1),
- (5,18,'2017-02-09',1),
- (6,4,'2017-02-11',1),
- (7,5,'2017-02-11',0),
- (8,6,'2017-02-11',1),
- (9,15,'2017-02-11',0),
- (10,18,'2017-02-11',1),
- (11,4,'2017-02-10',1),
- (12,5,'2017-02-10',1),
- (13,6,'2017-02-10',1),
- (14,15,'2017-02-10',1),
- (15,18,'2017-02-10',1),
- (16,4,'2017-02-10',1),
- (17,5,'2017-02-10',1),
- (18,6,'2017-02-10',2),
- (19,15,'2017-02-10',1),
- (20,18,'2017-02-10',1);
-CREATE TABLE `Assignment` (
+INSERT INTO `Attendance` (ID,ID_Student,Date,Presence) VALUES (1,4,'2007-10-02',1),
+ (2,5,'2007-10-02',0),
+ (3,6,'2007-10-02',0),
+ (4,4,'2007-10-03',0),
+ (5,5,'2007-10-03',1),
+ (6,6,'2007-10-03',0),
+ (7,4,'2007-10-04',1),
+ (8,5,'2007-10-04',1),
+ (9,6,'2007-10-04',0),
+ (10,4,'2007-10-04',1),
+ (11,5,'2007-10-04',0),
+ (12,6,'2007-10-04',0),
+ (13,4,'2017-02-10',0),
+ (14,5,'2017-02-10',1),
+ (15,6,'2017-02-10',1),
+ (65,18,'2017-03-09',1),
+ (66,6,'2017-03-09',1),
+ (67,5,'2017-03-09',1),
+ (71,18,'2017-03-10',1),
+ (72,5,'2017-03-10',0),
+ (73,6,'2017-03-10',2);
+CREATE TABLE "Assignment" (
 	`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`Name`	TEXT,
 	`Type`	TEXT,
 	`Max_points`	INTEGER,
-	`Delivery_date`	NUMERIC,
+	`Delivery_date`	TEXT,
 	`Content`	TEXT
 );
-INSERT INTO `Assignment` (ID,Name,Type,Max_points,Delivery_date,Content) VALUES (1,'new ass','individual',48,'2017-01-02','Content for assignment'),
- (2,'Python','group',24,'2017-12-12','Content about python'),
- (3,'Java','group',48,'2017-01-01','Java content'),
- (4,'Javascript','individual',48,'2017-03-05','Content Javascript');
+INSERT INTO `Assignment` (ID,Name,Type,Max_points,Delivery_date,Content) VALUES (2,'Python','individual',42,'2017-04-01','Basic content for Python'),
+ (3,'Javascript','individual',48,'2017-03-21','Content Javascript'),
+ (4,'Python','individual',48,'2017-04-23','Python content'),
+ (6,'nowy','individual',23,'2017-03-24','content assignment');
 COMMIT;
---
+++ models/db_alchemy.py
@@ -0,0 +1,76 @@
+from main import db
+
+
+class AssignmentDb(db.Model):
+    __tablename__ = 'Assignment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.String(10))
+    max_points = db.Column(db.Integer)
+    delivery_date = db.Column(db.String(10))
+    content = db.Column(db.String(200))
+
+class AttendanceDb(db.Model):
+    __tablename__ = 'Attendance'
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_student = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.String(10), nullable=False)
+    presence = db.Column(db.String(1), nullable=False)
+
+class CheckpointAssignmentDb(db.Model):
+    __tablename__ = 'Checkpoint_assignment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    assignment = db.Column(db.String(80))
+
+class CheckpointSubmissionDb(db.Model):
+    __tablename__ = 'Checkpoint_submission'
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_student = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    id_mentor = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    id_assignment = db.Column(db.Integer, db.ForeignKey('Assignment.id'))
+    date = db.Column(db.String(10))
+    card = db.Column(db.String(10))
+
+class SubmissionDb(db.Model):
+    __tablename__ = 'Submission'
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_student = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    id_assignment = db.Column(db.Integer, db.ForeignKey('Assignment.id'), nullable=False)
+    id_mentor = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    result = db.Column(db.String(200))
+    grade = db.Column(db.Integer)
+    date = db.Column(db.String(10))
+
+class TeamDb(db.Model):
+    __tablename__ = 'Team'
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_student = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+
+class UserDb(db.Model):
+    __tablename__ = 'User'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    surname = db.Column(db.String(30), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    birth_date = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    login = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False, unique=True)
+    user_type = db.Column(db.String(30), nullable=False)
+    student_checkpoint_submission = db.relationship('CheckpointSubmissionDb', backref='student_checkpoint_submission_ref', lazy="dynamic",
+                              foreign_keys='CheckpointSubmissionDb.id_student')
+    mentor_checkpoint_submission = db.relationship('CheckpointSubmissionDb', backref='mentor_checkpoint_submission_ref', lazy="dynamic",
+                             foreign_keys='CheckpointSubmissionDb.id_mentor')
+    student_submission = db.relationship('SubmissionDb', backref='student_submission_ref', lazy="dynamic",
+                              foreign_keys='SubmissionDb.id_student')
+    mentor_submission = db.relationship('SubmissionDb', backref='mentor_submission_ref', lazy="dynamic",
+                             foreign_keys='SubmissionDb.id_mentor')
